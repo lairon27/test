f90c8c5171ae2c87cd0693d293bdd36bab95d6a7
lairon27 <74205672+lairon27@users.noreply.github.com>
2021-11-18 17:53:38 +0200

    Add files via upload

---
+++ Models/AccountContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Account.Models
+{
+    public partial class AccountContext : DbContext
+    {
+        public AccountContext()
+        {
+        }
+
+        public AccountContext(DbContextOptions<AccountContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contacts>(entity =>
+            {
+                entity.HasKey(e => e.ContactId)
+                    .HasName("PK__contacts__B40CC6CD73B27013");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(20)
+                    .IsUnicode(true);
+
+                entity.Property(e => e.IncDesc)
+                    .IsRequired()
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Contacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Account.Models
+{
+    public class Contacts
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string IncDesc { get; set; }
+    }
+}
